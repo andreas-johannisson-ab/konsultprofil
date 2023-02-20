3aeb20275f5c7591e9990bcb711b05dc506d713f
Andreas Johannisson AB <andreas.johannisson.ab@gmail.com>
2023-02-20 16:41:01 +0100

    Uppdaterat texter.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,15 @@
 <template>
   <h1 class="gradient">{{ $t("name") }}</h1>
   <h2>{{ $t('title') }}</h2>
-  <p>Som person är jag, som är skriven i VueJS</p>
-  <p>Källkod för konsultprofilen:<br><a class="gradient" target="_blank" :href="$t('github')">{{ $t('github') }}</a></p>
+  <p>
+    Som person är jag en driven utvecklare som tycker om att jobba i team som kan hålla hög fart och kvalitet.
+
+    Jag tycker om att jobbar proaktivt med CI/CD och jag försöker löpande se möjligheter till förenklingar och förbättringar i utvecklingsarbetet.
+  </p>
+  <p>
+    För mig är det viktigt att mina kollegor ska känna att de har förtroende för mig. 
+    Jag tycker det är roligt att kunna hjälpa till när någon har problem och är själv inte rädd för att be om hjälp när det behövs.
+  </p>
 </template>
 
 <style scoped>
